26dde2192e77aa731a8667f5c3923b4597679590
N1K0232 <ns.nicolasilvestri@libero.it>
2023-11-07 21:22:16 +0100

    Add console app sample

---
+++ samples/WeatherForecastConsoleApp/Application.cs
@@ -0,0 +1,27 @@
+﻿using WeatherForecastClient;
+
+namespace WeatherForecastConsoleApp;
+
+public class Application
+{
+    private readonly IWeatherForecastClient weatherForecastClient;
+
+    public Application(IWeatherForecastClient weatherForecastClient)
+    {
+        this.weatherForecastClient = weatherForecastClient;
+    }
+
+    public async Task ExecuteAsync()
+    {
+        Console.Write("Search the weather for your city: ");
+        var city = Console.ReadLine() ?? string.Empty;
+
+        var weatherResponse = await weatherForecastClient.SearchAsync(city);
+        if (weatherResponse.IsSuccess)
+        {
+            Console.WriteLine(weatherResponse.CurrentWeather!.Name);
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ samples/WeatherForecastConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WeatherForecastClient;
+using WeatherForecastConsoleApp;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(ConfigureServices)
+    .Build();
+
+var application = host.Services.GetRequiredService<Application>();
+await application.ExecuteAsync();
+
+void ConfigureServices(HostBuilderContext hostingContext, IServiceCollection services)
+{
+    services.AddWeatherClient(options =>
+    {
+        options.ApiKey = "e715cb25b6c9421afc6e16ff968e9430";
+    });
+
+    services.AddSingleton<Application>();
+}
