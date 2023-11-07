2964aaf60048448683c634dcfe61b5be56c7022d
N1K0232 <ns.nicolasilvestri@libero.it>
2023-11-07 21:00:36 +0100

    Add missing model for response serialization

---
+++ src/WeatherForecastClient/Models/WeatherResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace WeatherForecastClient.Models;
+
+public class WeatherResponse
+{
+    private WeatherResponse(CurrentWeather? currentWeather, Error? error)
+    {
+        CurrentWeather = currentWeather;
+        Error = error;
+    }
+
+    public CurrentWeather? CurrentWeather { get; }
+
+    public Error? Error { get; }
+
+    public bool IsSuccess => CurrentWeather is not null && Error is null;
+
+    public static WeatherResponse Ok(CurrentWeather? currentWeather) => new(currentWeather, null);
+
+    public static WeatherResponse Fail(Error? error) => new(null, error);
+}
