08f8adc45304433e437c4821c11e4caa8aeb1eeb
N1K0232 <ns.nicolasilvestri@libero.it>
2023-11-07 21:00:19 +0100

    Add handlers for the http request and polly retry policy

---
+++ src/WeatherForecastClient/Handlers/QueryStringInjectorHttpMessageHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+
+namespace WeatherForecastClient.Handlers;
+
+internal class QueryStringInjectorHttpMessageHandler : DelegatingHandler
+{
+    public Dictionary<string, string> Parameters { get; }
+
+    public QueryStringInjectorHttpMessageHandler(Dictionary<string, string> parameters = null, HttpMessageHandler innerHandler = null)
+          : base(innerHandler ?? new HttpClientHandler())
+    {
+        Parameters = parameters ?? new Dictionary<string, string>();
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var parameters = HttpUtility.ParseQueryString(request.RequestUri!.Query);
+
+        foreach (var parameter in Parameters)
+        {
+            parameters.Add(parameter.Key, parameter.Value);
+        }
+
+        var uriBuilder = new UriBuilder(request.RequestUri!)
+        {
+            Query = parameters.ToString()
+        };
+
+        request.RequestUri = new Uri(uriBuilder.ToString());
+        return base.SendAsync(request, cancellationToken);
+    }
+}
---
+++ src/WeatherForecastClient/Handlers/TransientErrorDelegatingHandler.cs
@@ -0,0 +1,19 @@
+﻿using Polly.Registry;
+
+namespace WeatherForecastClient.Handlers;
+
+internal class TransientErrorDelegatingHandler : DelegatingHandler
+{
+    private readonly ResiliencePipelineProvider<string> pipelineProvider;
+
+    public TransientErrorDelegatingHandler(ResiliencePipelineProvider<string> pipelineProvider)
+    {
+        this.pipelineProvider = pipelineProvider;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var pipeline = pipelineProvider.GetPipeline<HttpResponseMessage>("http");
+        return await pipeline.ExecuteAsync(async token => await base.SendAsync(request, token), cancellationToken);
+    }
+}
