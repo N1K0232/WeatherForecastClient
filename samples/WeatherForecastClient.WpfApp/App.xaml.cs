859c5afd9b8d3d459386872c95cf0b8b683df679
N1K0232 <ns.nicolasilvestri@libero.it>
2023-11-17 21:51:38 +0100

    Add wpf sample

---
+++ samples/WeatherForecastClient.WpfApp/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WeatherForecastClient.Extensions;
+
+namespace WeatherForecastClient.WpfApp;
+
+public partial class App : Application
+{
+    private readonly IHost host;
+
+    public App()
+    {
+        host = Host.CreateDefaultBuilder()
+            .ConfigureServices(ConfigureServices)
+            .Build();
+    }
+
+    private void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+    {
+        services.AddWeatherClient(options =>
+        {
+            options.ApiKey = "";
+        });
+
+        services.AddSingleton<MainWindow>();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var window = host.Services.GetRequiredService<MainWindow>();
+        window.Show();
+
+        base.OnStartup(e);
+    }
+}
---
+++ samples/WeatherForecastClient.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+using WeatherForecastClient.Models;
+
+namespace WeatherForecastClient.WpfApp;
+
+public partial class MainWindow : Window
+{
+    private readonly IWeatherForecastService weatherForecastService;
+
+    public MainWindow(IWeatherForecastService weatherForecastService)
+    {
+        this.weatherForecastService = weatherForecastService;
+        InitializeComponent();
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+        ConditionCityTextBlock.Text = null;
+        ConditionImage.Source = null;
+        ConditionTextBlock.Text = null;
+        TemperatureTextBlock.Text = null;
+
+        if (!string.IsNullOrWhiteSpace(CityTextBox.Text))
+        {
+            var response = await weatherForecastService.SearchAsync(CityTextBox.Text);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var weather = new Weather(response.Content);
+                ConditionCityTextBlock.Text = weather.CityName;
+                ConditionImage.Source = new BitmapImage(new Uri(weather.ConditionIconUrl));
+                ConditionTextBlock.Text = weather.Condition;
+                TemperatureTextBlock.Text = $"{weather.Temperature} °C";
+            }
+            else
+            {
+                var error = await response.Error.GetContentAsAsync<Error>();
+                MessageBox.Show($"Unable to retrieve weather codition for {CityTextBox.Text}: {error.Message}.", "Weather Client", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
